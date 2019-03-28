05f62ae0a126253dbf6866541e71d158badaba70
t-kowalewski <wkowalewski@outlook.com>
2019-03-28 13:31:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+  const number = document.querySelector('#number').value;
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      
+      let output = '';
+  
+      if(response.type === 'success') {
+        response.value.forEach(function(arrItem) {
+          output += `<li>${arrItem.joke}</li>`;
+        });
+      }
+      else {
+        output += '<li>Sorry, something went wrong</li>';
+      }
+
+      document.querySelector('.jokes').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+
+  e.preventDefault(); /* it's a button */
+}
